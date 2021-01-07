6cb87fcc5c30c2af756b6bc7ad44764205af8137
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-06 21:47:21 -0800

    fixed syntax error

--- src/App.js
+++ src/App.js
@@ -7,26 +7,27 @@ const App = () => {
   const columns = [
     {
       Header: 'First Name',
-      accessor: 'first_name'
+      accessor: 'first_name',
+      filterable: true
     },
     {
       Header: 'Last Name',
-      accessor: 'last_name'
+      accessor: 'last_name',
       filterable: true
     },
     {
       Header: 'Email',
-      accessor: 'email'
+      accessor: 'email',
       filterable: true
     },
     {
       Header: 'Phone Number',
-      accessor: 'phone_number'
+      accessor: 'phone_number',
       filterable: true
     },
     {
       Header: 'Department',
-      accessor: 'department'
+      accessor: 'department',
       filterable: true
     },
   ]
